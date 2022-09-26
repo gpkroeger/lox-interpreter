cb6f6b8d61346eca81e79bd25bff0d5d7e83b95d
oyrussell <oyrussell@crimson.ua.edu>
2022-09-26 12:37:18 -0500

    Minor Update

--- parser.py
+++ parser.py
@@ -1,9 +1,13 @@
+import Token, tokenTypes, expression, statements
+
 class parser:
-    def __init__(self):
-        self.finalTokens = []
-        self.current = 0
-    
-    def parseTokens(self, tokens):
+    def __init__(self, tokens):
         self.finalTokens = tokens
-    
+        self.current = 0
+
+    def peek(self):
+        return self.tokens[self.current]
+
+    def isEnd(self):
+        return self.peek().type == tokenTypes.EOF
     
