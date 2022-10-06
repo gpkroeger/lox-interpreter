ac6e7433590d977ac5a47396632e490b0321df36
oyrussell <oyrussell@crimson.ua.edu>
2022-10-06 11:19:52 -0500

    Adding Parse Function, and starting declarations

--- parser.py
+++ parser.py
@@ -1,10 +1,31 @@
+from lib2to3.pgen2 import token
 from Token import Token
 from tokenTypes import tokenTypes
+
+class parseError(Exception):
+    pass
 
 class parser:
     def __init__(self, tokens):
         self.finalTokens = tokens
         self.current = 0
+
+    def parse(self):
+        statements = []
+        while not self.isEnd():
+            statements.append(self.declaration())
+        return statements
+    def declaration(self):
+        try:
+            if self.match(tokenTypes.CLASS):
+                return self.classDeclaration()
+            if self.match(tokenTypes.FUN):
+                return self.function("function")
+            if self.match(tokenTypes.VAR):
+                return self.varDeclaration()
+            return self.statement()
+        except parseError:
+            return None
 
     def peek(self):
         return self.tokens[self.current]
@@ -12,7 +33,7 @@ class parser:
     def isEnd(self):
         return self.peek().type == tokenTypes.EOF
 
-    def expr(self):
-        return equality()
+    #def expr(self):
+    #    return equality()
     
     
